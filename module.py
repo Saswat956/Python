3d4bdcf94aa503e531565967f697e79250d44835
Saswat Swain <64731852+Saswat956@users.noreply.github.com>
2020-08-18 16:34:41 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,28 @@
+def Add(a,b):
+    """
+    This is teh help doc of Add()
+    skdhfsd
+    fsdhfsdhggs
+    
+    """
+    print(a+b)
+
+def Sub(a,b):
+    print(a-b)
+
+def Prod(a,b):
+    print(a*b)
+
+def Pow(a,b):
+    print(a**b)
+
+def Div(a,b):
+    print(a/b)
+
+def __testFunc(a,b):
+    print(a+b)
+
+print("I am getting executed from",__name__)
+if __name__ == "__main__":
+    Add(2,3)
+    Prod(10,2)
---
+++ module_test.py
@@ -0,0 +1,8 @@
+#import module
+#import module as m
+#from module import *
+from module import Add,Prod
+#print(dir())
+#Add(2,3)
+#print(dir(m))
+print(Add.__doc__)
